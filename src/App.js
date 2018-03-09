30c1560bcfe98ef7b56650fbc0a65d678570faaf
Ridima14 <ahuja.ridima14@gmail.com>
2018-03-09 15:29:31 +0530

    Add form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';//bmj
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );// maro
+      <form >
+      <label>Username:
+      <input type="text" value=""/>
+      </label>
+      <label>Password:
+      <input type="password" value=""/>
+      </label>
+      </form>
+    );
   }
 }
 
